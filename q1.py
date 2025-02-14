74ab7aab09586b85fc5169be6e5b43555218e429
dakshcodez <daksharoracodez@gmail.com>
2025-02-14 14:08:56 +0530

    solved 1st problem

---
+++ q1.py
@@ -0,0 +1,18 @@
+#LONG WORDS
+
+def convert(word):
+    if len(word) <= 10:  #if length of word is lesser than 10, the word remains unchanged
+        return word
+    else:   #concatenate the first and last element with length of word - 2 in middle
+        return word[0] + str(len(word) - 2) + word[-1]  
+    
+# taking input
+
+def main():
+    t = int(input())
+    for i in range(t):
+        word = input()
+        print(convert(word))
+
+if __name__ == "__main__":
+    main()
